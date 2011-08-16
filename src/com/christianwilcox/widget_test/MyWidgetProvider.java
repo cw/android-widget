b714244093545282c1d9ca3f565248b3f943a27b
Christian Wilcox <cw@christianwilcox.com>
2011-08-16 10:15:47 -0500

    adjusted line formatting

--- src/com/christianwilcox/widget_test/MyWidgetProvider.java
+++ src/com/christianwilcox/widget_test/MyWidgetProvider.java
@@ -10,22 +10,20 @@ import android.widget.RemoteViews;
 public class MyWidgetProvider extends AppWidgetProvider {
 
   @Override
-  public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-    int[] appWidgetIds) {
-     final int N = appWidgetIds.length;
-     // Create some random data
-     String fakeUpdate = "Random: ";
-     Random random = new Random();
-     int nextInt = random.nextInt(100);
-     fakeUpdate += String.valueOf(nextInt);
-     for (int i=0; i<N; i++) {
-       int widgetId = appWidgetIds[i];
-       int number = (new Random().nextInt(100));
+  public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+    final int N = appWidgetIds.length;
+    // Create some random data
+    String fakeUpdate = "Random: ";
+    Random random = new Random();
+    int nextInt = random.nextInt(100);
+    fakeUpdate += String.valueOf(nextInt);
+    for (int i=0; i<N; i++) {
+      int widgetId = appWidgetIds[i];
+      int number = (new Random().nextInt(100));
 
-       RemoteViews views = new RemoteViews(context.getPackageName(),
-	R.layout.widget_layout);
-       views.setTextViewText(R.id.TextView01, String.valueOf(number));
-       appWidgetManager.updateAppWidget(widgetId, views);
-     }
+      RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+      views.setTextViewText(R.id.TextView01, String.valueOf(number));
+      appWidgetManager.updateAppWidget(widgetId, views);
+    }
   }	
 }
