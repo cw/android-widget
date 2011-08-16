e61500634505f36cb7b1fc1f0a1f61111d9b3b7f
Christian Wilcox <cw@christianwilcox.com>
2011-08-16 07:19:16 -0500

    initial checkin

---
+++ src/com/christianwilcox/widget_test/MyWidgetProvider.java
@@ -0,0 +1,31 @@
+package com.christianwilcox.widget_test;
+
+import java.util.Random;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+public class MyWidgetProvider extends AppWidgetProvider {
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		// Create some random data
+		String fakeUpdate = "Random: ";
+		Random random = new Random();
+		int nextInt = random.nextInt(100);
+		fakeUpdate += String.valueOf(nextInt);
+		for (int i : appWidgetIds) {
+			int widgetId = appWidgetIds[i];
+			int number = (new Random().nextInt(100));
+
+			RemoteViews views = new RemoteViews(context.getPackageName(),
+					R.layout.widget_layout);
+			views.setTextViewText(R.id.TextView01, String.valueOf(number));
+			appWidgetManager.updateAppWidget(widgetId, views);
+		}
+	}
+	
+}
